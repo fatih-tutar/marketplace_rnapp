665c1884928f7d02278cfbcdce487cea22ea4a0a
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2022-10-19 11:21:35 +0300

    Intro

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { Text, View, SafeArea, Flatlist } from 'react-native';
+
+const Home = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+export default Home
